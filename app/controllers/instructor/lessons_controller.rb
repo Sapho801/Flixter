eb0a3f176ae218722d421be5d4f033d8466314f7
Ohene Bonsu <oheneb.bonsu@gmail.com>
2019-09-10 16:42:32 +0000

    Completed Lesson 36: New lesson form modal

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,17 +1,10 @@
 class Instructor::LessonsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_section, only: [:new, :create]
+  before_action :require_authorized_for_current_section, only: [:create]
   before_action :require_authorized_for_current_lesson, only: [:update]
 
-  def require_authorized_for_current_lesson
-    if current_lesson.section.course.user != current_user
-      render plain: 'Unauthorized', status: :unauthorized
-    end
-  end
 
-  def new
-    @lesson = Lesson.new
-  end
+  
 
   def create
     @lesson = current_section.lessons.create(lesson_params)
@@ -23,7 +16,18 @@ class Instructor::LessonsController < ApplicationController
     render plain: 'updated!'
   end
 
+
+
+
+
+
   private
+
+  def require_authorized_for_current_lesson
+    if current_lesson.section.course.user != current_user
+      render plain: 'Unauthorized', status: :unauthorized
+    end
+  end
 
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
